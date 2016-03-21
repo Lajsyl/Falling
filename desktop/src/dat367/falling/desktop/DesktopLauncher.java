fe4e734dc6769e3c69f69717ca7e115d03d27aef
Simon Moos <simon.moos95@gmail.com>
2016-03-21 16:14:21 +0100

    Initial commit

---
+++ desktop/src/dat367/falling/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dat367.falling.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dat367.falling.Falling;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Falling(), config);
+	}
+}
