ccc892629422a0fc452d134b3a28f7307af130c3
HvE <hannes.von.essen@gmail.com>
2016-03-23 17:17:06 +0100

    Try adding VR extension

--- android/src/dat367/falling/AndroidLauncher.java
+++ android/src/dat367/falling/AndroidLauncher.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.CardBoardAndroidApplication;
+import com.badlogic.gdx.backends.android.CardBoardApplicationListener;
 import dat367.falling.Falling;
 
-public class AndroidLauncher extends AndroidApplication {
+
+public class AndroidLauncher extends CardBoardAndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
--- core/src/dat367/falling/Falling.java
+++ core/src/dat367/falling/Falling.java
@@ -2,11 +2,13 @@ package dat367.falling;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.android.CardBoardAndroidApplication;
+import com.badlogic.gdx.backends.android.CardBoardApplicationListener;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Falling extends ApplicationAdapter {
+public class Falling implements CardBoardApplicationListener {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -17,11 +19,56 @@ public class Falling extends ApplicationAdapter {
 	}
 
 	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public void onNewFrame(com.google.vrtoolkit.cardboard.HeadTransform paramHeadTransform) {
+
+	}
+
+	@Override
+	public void onDrawEye(com.google.vrtoolkit.cardboard.Eye paramEye) {
+
+	}
+
+	@Override
+	public void onFinishFrame(com.google.vrtoolkit.cardboard.Viewport paramViewport) {
+
+	}
+
+	@Override
+	public void onRendererShutdown() {
+
+	}
+
+	@Override
+	public void onCardboardTrigger() {
+
 	}
 }
