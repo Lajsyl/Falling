f316dcd91405d95736374f453c20f454eee77696
Simon Moos <simon.moos95@gmail.com>
2016-04-11 13:23:30 +0200

    Remove asLibGdxVector to separate platform from the rest

--- core/src/dat367/falling/Vector.java
+++ core/src/dat367/falling/Vector.java
@@ -1,7 +1,5 @@
 package dat367.falling;
 
-import com.badlogic.gdx.math.Vector3;
-
 public class Vector {
 
     private float x, y, z;
@@ -42,10 +40,6 @@ public class Vector {
 
     float dot(Vector other) {
         return x*other.x + y*other.y + z*other.z;
-    }
-
-    Vector3 asLibGdxVector() {
-        return new Vector3(x, y, z);
     }
 
 }
