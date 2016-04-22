d7925e3375bf0b4bcf122426b369a4547ee2f974
Timbertea <sabrina.sabbe@hotmail.com>
2016-04-22 14:33:15 +0200

    actually add LandedState class this time

---
+++ core/src/dat367/falling/core/LandedState.java
@@ -0,0 +1,15 @@
+package dat367.falling.core;
+
+
+public class LandedState implements FallState {
+
+    @Override
+    public void setup(Jumper jumper) {
+
+    }
+
+    @Override
+    public FallState handleFalling(float deltaTime, Jumper jumper) {
+        return null;
+    }
+}
