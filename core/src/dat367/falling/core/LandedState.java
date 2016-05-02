04386833d6c75041a70e15bb4fdd7530a860b36d
Lisa Linnéa Karlsson <lisaenajs@gmail.com>
2016-05-02 11:48:05 +0200

    implement handleFalling

--- core/src/dat367/falling/core/LandedState.java
+++ core/src/dat367/falling/core/LandedState.java
@@ -14,11 +14,30 @@ public class LandedState implements FallState {
     //Might create a change in postion as to mimic the shock when landing
     @Override
     public FallState handleFalling(float deltaTime, Jumper jumper) {
+        Vector v0 = jumper.getVelocity();
 
+        jumper.setAcceleration(calculateAcceleration(jumper));
+        jumper.setVelocity(calculateVelocity(deltaTime, jumper));
+        jumper.setPosition(calculatePosition(deltaTime, jumper, v0));
 
         return null;
     }
 
+    public Vector calculateAcceleration(Jumper jumper){
+        Vector targetVelocity = new Vector(0,0,0);
+        Vector currentVelocity = jumper.getVelocity();
+        currentVelocity = currentVelocity.projectedXZ();
+        Vector newAcc = targetVelocity.sub(currentVelocity);
 
+        return newAcc.scale(10f);
+    }
 
+    public Vector calculateVelocity(float deltaTime, Jumper jumper){
+        return jumper.getVelocity().add(jumper.getAcceleration().scale(deltaTime));
+    }
+
+    public Vector calculatePosition(float deltaTime, Jumper jumper, Vector v0){
+        Vector v = v0.add(jumper.getVelocity().scale(deltaTime/2));
+        return v.add(jumper.getPosition());
+    }
 }
