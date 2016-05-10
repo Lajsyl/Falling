e5ce7df47baa8e0e333e6f03605c92ab4268c8f6
Lisa Linnéa Karlsson <lisaenajs@gmail.com>
2016-05-10 16:35:57 +0200

    implement GUITextTasks and write balloons when landed

--- core/src/dat367/falling/core/BalloonGameMode.java
+++ core/src/dat367/falling/core/BalloonGameMode.java
@@ -1,6 +1,9 @@
 package dat367.falling.core;
 
 import dat367.falling.math.Vector;
+import dat367.falling.platform_abstraction.GUITask;
+import dat367.falling.platform_abstraction.GUITextTask;
+import dat367.falling.platform_abstraction.RenderQueue;
 import dat367.falling.platform_abstraction.ResourceRequirements;
 
 import java.util.ArrayList;
@@ -9,19 +12,27 @@ import java.util.List;
 public class BalloonGameMode implements GameMode {
 
     protected int collectibleCount = 0;
+    protected int numberOfBalloons = 0;
     protected List<Collectible> balloonList;
+    private boolean renderText = false;
 
     public BalloonGameMode(ResourceRequirements resourceRequirements){
         NotificationManager.addObserver(CollisionManager.collectibleCollisionId, new NotificationManager.EventHandler<CollisionManager.CollisionData>() {
             @Override
             public void handleEvent(NotificationManager.Event<CollisionManager.CollisionData> event) {
                 collectibleCount++;
-                System.out.println("Baloons: " + collectibleCount);
                 if(event.data.getObject1().getName().equals(CollisionManager.collectibleCollisionId)){
                     event.data.getObject1().setEnabled(false);
                 }else{
                     event.data.getObject2().setEnabled(false);
                 }
+            }
+        });
+        NotificationManager.addObserver(LandedState.playerHasStopped, new NotificationManager.EventHandler<Object>() {
+
+            @Override
+            public void handleEvent(NotificationManager.Event<Object> event) {
+                renderText = true;
             }
         });
 
@@ -31,12 +42,18 @@ public class BalloonGameMode implements GameMode {
             float z = (float)Math.sin((i-1000)/2500f*10*Math.PI)*75;
             Collectible c = new Collectible(resourceRequirements, new Vector(x,i,z));
             balloonList.add(c);
+            numberOfBalloons++;
         }
     }
 
     public void update(float deltaTime){
         for (Collectible c : balloonList){
             c.update(deltaTime);
+        }
+
+        if(renderText) {
+            String text = "You cought " + collectibleCount + " of " + numberOfBalloons;
+            RenderQueue.addGUITask(new GUITextTask(text, new Vector(1, 0, 0), new Vector(0.5f, 0.5f, .5f), true));
         }
     }
 }
--- core/src/dat367/falling/platform_abstraction/RenderQueue.java
+++ core/src/dat367/falling/platform_abstraction/RenderQueue.java
@@ -7,25 +7,27 @@ import java.util.Queue;
 public class RenderQueue {
 
     private static Queue<RenderTask> queue = new LinkedList<RenderTask>();
+    private static Queue<GUITask> guiQueue = new LinkedList<GUITask>();
 
     public static void addTask(RenderTask task) {
         queue.add(task);
     }
 
-    public static RenderTask getNextTask() {
-        return queue.remove();
-    }
-
-    public static boolean hasMoreTasks() {
-        return !queue.isEmpty();
+    public static void addGUITask(GUITask guiTask){
+        guiQueue.add(guiTask);
     }
 
     public static void clear() {
         queue.clear();
+        guiQueue.clear();
     }
 
     public static Iterable<RenderTask> getTasks() {
         return Collections.unmodifiableCollection(queue);
+    }
+
+    public static Iterable<GUITask> getGUITasks() {
+        return Collections.unmodifiableCollection(guiQueue);
     }
 
 }
