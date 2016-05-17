c733b8f5d7c0e9d3b0073f7db9bdf78925e2e9c2
Timbertea <sabrina.sabbe@hotmail.com>
2016-05-17 11:59:54 +0200

    add tests for rotation class

---
+++ core/test/RotationTest.java
@@ -0,0 +1,52 @@
+import dat367.falling.math.Rotation;
+import dat367.falling.math.Vector;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class RotationTest {
+
+    @Test
+    public void testGetDirection() throws Exception {
+        Rotation r = new Rotation(new Vector(1,17,3), new Vector(2,16,78));
+
+        assertTrue(r.getDirection().equals(new Vector (1,17,3)));
+    }
+
+    @Test
+    public void testGetUp() throws Exception {
+        Rotation r = new Rotation(new Vector(1,17,3), new Vector(2,16,78));
+
+        assertTrue(r.getUp().equals(new Vector (2,16,78)));
+    }
+
+    @Test
+    public void testGetRight() throws Exception {
+        Rotation r = new Rotation(new Vector(1,0,0), new Vector(0,1,0));
+
+        assertTrue(r.getRight().equals(new Vector(0,0,1)));
+    }
+
+    @Test
+    public void testRotate() throws Exception {
+        Rotation r = new Rotation(new Vector(1,0,0), new Vector(0,1,0));
+
+        r.rotate(new Vector(0,1,0), (float)Math.PI/2);
+
+        //TODO account for inexactness caused by rounding errors
+        assertTrue(r.equals(new Rotation(new Vector(0,0,-1), new Vector(0,1,0))));
+    }
+
+    @Test
+    public void testRotate1() throws Exception {
+        Rotation r = new Rotation(new Vector(1,0,0), new Vector(0,1,0));
+        r.rotate(new Rotation(new Vector(0,1,0), new Vector(0,0,1)));
+
+        //TODO is this even how it is supposed to work?
+        assertTrue(r.equals(new Rotation(new Vector(0,1,0), new Vector(0,0,1))));
+
+    }
+
+
+}
