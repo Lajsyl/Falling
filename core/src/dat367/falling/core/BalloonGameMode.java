c9ce29487fe73027439196d3f4cebb79bd65bb9b
Lisa Linnéa Karlsson <lisaenajs@gmail.com>
2016-05-19 16:45:43 +0200

    add simple level

--- core/src/dat367/falling/core/BalloonGameMode.java
+++ core/src/dat367/falling/core/BalloonGameMode.java
@@ -14,7 +14,7 @@ public class BalloonGameMode implements GameMode {
     private final int totalBalloonCount = 25;
     private List<Collectible> balloonList = new ArrayList<Collectible>(totalBalloonCount);
     private final int totalObstacleCount = 25;
-    private List<Obstacle> obstacleList = new ArrayList<Obstacle>(totalObstacleCount);
+    private List<Obstacle> obstacleList = new ArrayList<Obstacle>(totalObstacleCount*2);
 
     private int balloonCombo = 0;
     private int score = 0;
@@ -63,15 +63,30 @@ public class BalloonGameMode implements GameMode {
         }
 
         for (int i = 0; i < totalObstacleCount; i++){
+
             float step = (float) i;
-            float x = (float)Math.cos(step) * 30 + 100;
-            float z = (float)Math.sin(step) * 30 + 100;
 
             // Every 100 meters from 1000m and up.
-            float y = step * 100.0f + 1000.0f + 100;
-
-            Obstacle o = new Obstacle(resourceRequirements, new Vector(x, y, z));
-            obstacleList.add(o);
+            if(i < 15) {
+                float y1 = step * 100.0f + 1000.0f - 25;
+                float y2 = step * 100.0f + 1000.0f + 25;
+                float x = (float)Math.cos(step) * 60;
+                float z = (float)Math.sin(step) * 60;
+                Obstacle o1 = new Obstacle(resourceRequirements, new Vector(x, y1, z));
+                Obstacle o2 = new Obstacle(resourceRequirements, new Vector(x, y2, z));
+                obstacleList.add(o1);
+                obstacleList.add(o2);
+            }else{
+                float y = step * 100.0f + 1000.0f;
+                float x1 = (float)Math.cos(step) * 60 + 10;
+                float x2 = (float)Math.cos(step) * 60 - 10;
+                float z1 = (float)Math.sin(step) * 60 + 10;
+                float z2 = (float)Math.sin(step) * 60 - 10;
+                Obstacle o1 = new Obstacle(resourceRequirements, new Vector(x1, y, z1));
+                Obstacle o2 = new Obstacle(resourceRequirements, new Vector(x2, y, z2));
+                obstacleList.add(o1);
+                obstacleList.add(o2);
+            }
         }
     }
 
@@ -89,8 +104,12 @@ public class BalloonGameMode implements GameMode {
     private void obstacleCollision(CollisionManager.CollisionData collisionData) {
         collisionData.getOtherObject().setEnabled(false);
         collisionData.getOtherObject().setParentEnabled(false);
-        balloonCombo = 0;
-        System.out.println("mine" + balloonCombo);
+        if (balloonCombo < 15) {
+            balloonCombo = 0;
+        }else{
+            balloonCombo -= 15;
+        }
+        System.out.println("mine hit");
     }
 
     public void update(float deltaTime) {
