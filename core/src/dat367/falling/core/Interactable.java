31c5bf1d79b0033ce3003f365c4bca84b1b5717f
Lisa Linnéa Karlsson <lisaenajs@gmail.com>
2016-05-25 16:12:48 +0200

    add abstract class Interactable

---
+++ core/src/dat367/falling/core/Interactable.java
@@ -0,0 +1,40 @@
+package dat367.falling.core;
+
+
+import dat367.falling.math.Rotation;
+import dat367.falling.math.Vector;
+import dat367.falling.platform_abstraction.Model;
+import dat367.falling.platform_abstraction.ModelRenderTask;
+import dat367.falling.platform_abstraction.RenderQueue;
+import dat367.falling.platform_abstraction.ResourceRequirements;
+
+public abstract class Interactable implements Positioned{
+
+    private Collider collider;
+    private Vector position;
+
+    private boolean enabled = true;
+
+
+    @Override
+    public Vector getPosition() {
+        return position;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public abstract String getPositionChangedEventID();
+
+    public abstract String getInteractableID();
+
+    public void setPosition(Vector position) {
+        this.position = position;
+        NotificationManager.registerEvent(getPositionChangedEventID(), this);
+    }
+
+    public abstract void update(float deltaTime);
+}
