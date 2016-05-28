0860100621de283891c17de3aae2d7834c715de0
HvE <hannes.von.essen@gmail.com>
2016-05-29 00:11:40 +0200

    Improve framerate in airplane

--- core/src/dat367/falling/core/DefaultCloudSimulationConfig.java
+++ core/src/dat367/falling/core/DefaultCloudSimulationConfig.java
@@ -15,9 +15,15 @@ public class DefaultCloudSimulationConfig implements CloudSimulator.CloudSimulat
 
     @Override
     public int getCloudAmountForHeight(float height, int maxAmountOfClouds) {
-        // Only clouds in height 1000m-4000m, and most at 4000.
-        float multiple = FallingMath.smoothstep(4000, 1000, height);
-        return Math.round((1.0f - multiple) * maxAmountOfClouds);
+        if (height > 3650) {
+            // Render very few clouds around plane (=3700m) for better initial frame rate
+            float multiple = FallingMath.smoothstep(3750, 3650, height);
+            return Math.round((multiple) * maxAmountOfClouds);
+        } else {
+            // Only clouds in height 1000m-3650m, and most at 3650.
+            float multiple = FallingMath.smoothstep(3650, 1000, height);
+            return Math.round((1.0f - multiple) * maxAmountOfClouds);
+        }
     }
 
     @Override
